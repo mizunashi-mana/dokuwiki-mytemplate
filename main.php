e45159000bb71c026aa332d455a48eb149b977c0
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2015-05-20 20:02:58 +0900

    add translation bar

--- main.php
+++ main.php
@@ -37,7 +37,11 @@ $showSidebar = $hasSidebar && ($ACT=='show');
             <?php if($showSidebar): ?>
                 <!-- ********** ASIDE ********** -->
                 <div id="dokuwiki__aside"><div class="pad aside include group">
-                    <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
+                    <h3 class="toggle"><br></h3>
+                    <?php
+                        $translation = plugin_load('helper','translation');
+                        if ($translation) echo $translation->showTranslations();
+                    ?>
                     <div class="content">
                         <?php tpl_flush() ?>
                         <?php tpl_includeFile('sidebarheader.html') ?>
